00efb8dde013a61158625059d062c6e1e61085bc
arad <rakholl@gmail.com>
2017-12-08 10:30:17 +0200

    Varmistuscommit ennen testaamista laboratorion tietokoneilla

--- PiirturiVerkkoSivu.py
+++ PiirturiVerkkoSivu.py
@@ -6,6 +6,9 @@
 
 #Kun ohjelma on valmis julkiseen verkkoon: flask run --host=0.0.0.0
 
+#Palvelimeen saa debug-tilassa yhteyden kirjoittamalla selaimen
+#osoitekenttään localhost:5000
+
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory, make_response
 import webbrowser
 from werkzeug.utils import secure_filename
@@ -14,38 +17,13 @@ import subprocess
 #import pygame
 import sys
 
-
-
-#os.getcwd()
-#print(moduuliPolku)
-
 #Huom. moduulia svg2gcode on muokattu moduulipolkujen ja
 #tiedostopolkujen osalta soveltuvammaksi SeAMK:n ympäristöön
-#import svg2gcode
-
-#Debug-kategorioita voi kääntää päälle ja pois
-#tarpeen mukaan
-
-debugCategories = []
-
-#debugCategories.append("palvelinSisalto")
-debugCategories.append("tiedostonLahetys")
-debugCategories.append("nimenTarkistus")
-
-#Määritellään aluksi, ollaanko julkisessa verkossa,
-#(esimerkiksi debug-toiminnot eivät ole tietoturvallisia)
-
-def debug(msg, cat):
-    if cat in debugCategories:
-        print("DEBUG: "+str(msg))
-
 
 #Määritellään Flask-applikaatio-objekti
 piirt = Flask(__name__)
 
 online = True
-
-debug("Palvelimen sisältö: "+str(dir(piirt)),"palvelinSisalto")
 
 #HUOM! Muista kääntää flaskin debug pois päältä,
 #kun siirrytään julkiseen verkkoon
@@ -85,17 +63,14 @@ def profile(name):
     
     return render_template("TestiTervehdysKuva.html", name=name)
 
-def allowed_file(filename):
-    debug("sallitutTiedostoTyypit: "+str(sallitutTiedostoTyypit),"nimenTarkistus")
+def allowed_file(filename):    
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in sallitutTiedostoTyypit
 
 @piirt.route("/", methods=['GET', 'POST'])
 def upload_file():
-    if online == True:
-        debug("Server reacted","tiedostonLahetys")
-        if request.method == 'POST':
-            debug("Post received, request.files == "+str(request.files),"tiedostonLahetys")
+    if online == True:        
+        if request.method == 'POST':            
             # Tarkistetaan, onko POST-pyynnössä mukana tiedosto
             if 'file' not in request.files:
                 flash('No file part')
@@ -107,8 +82,7 @@ def upload_file():
                 return 'Ei valittua tiedostoa'
             #Tarkistetaan, onko tiedosto sallittua tyyppiä
             if file and allowed_file(file.filename):                
-                filename = secure_filename(file.filename)
-                debug("filename: "+str(filename),"nimenTarkistus")
+                filename = secure_filename(file.filename)                
                 #dump(file)
                 savPath = os.path.join(piirt.config['UPLOAD_FOLDER'], filename)
                 #Tallennetaan tiedosto paikallisesti määriteltyyn polkuun
